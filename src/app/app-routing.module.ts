d39f779f73c21a7e8ac57594c678a50684e47d38
Aaron <aaronzaru@gmail.com>
2023-05-05 22:04:03 +0200

    establecido el routing, para que vaya a la master-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MasterPageComponent } from './layout/master-page/master-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MasterPageComponent, // Ruta inicial, localhost:4200 y carga del master-page.
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
