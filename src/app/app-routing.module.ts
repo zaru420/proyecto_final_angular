732559f5d3c1878a76854edaf8495216bf051e80
Aaron <aaronzaru@gmail.com>
2023-05-05 23:18:24 +0200

    header responsive, con logo y los accesos a algunas páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MasterPageComponent } from './layout/master-page/master-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/errors/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '', component: MasterPageComponent, // Ruta inicial, localhost:4200 y carga del master-page.
-    
+    children: [
+      // hijos de la master page, si coincide los path los llama
+      { path: '', component: HomeComponent},
+      { path: 'login', component: LoginComponent},
+
+      // esta tiene que ir la ultima, la de errors
+      { path: '**', component: NotFoundComponent},
+    ]
   }
 ];
 
