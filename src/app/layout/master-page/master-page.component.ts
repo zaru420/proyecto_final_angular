0eaf19656adf58a7e37575d02a2abceb74e86a14
Aaron <aaronzaru@gmail.com>
2023-05-06 19:19:36 +0200

    login terminado

--- src/app/layout/master-page/master-page.component.ts
+++ src/app/layout/master-page/master-page.component.ts
@@ -1,32 +1,43 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-master-page',
   templateUrl: './master-page.component.html',
   styleUrls: ['./master-page.component.css']
 })
-export class MasterPageComponent {
-  showFooter: boolean = false;
+export class MasterPageComponent implements AfterViewInit {
+  @ViewChild('content') content!: ElementRef;
+  showFooter = false;
+  pageHeight = 0;
 
-  @HostListener("window:scroll", [])
-  onWindowScroll() {
-    const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    
-    if (scrollTop >= height) {
-      this.showFooter = true;
-    } else {
-      this.showFooter = false;
+  ngAfterViewInit() {
+    // Verificar si la referencia existe
+    if (this.content) {
+      // Obtener altura del contenido
+      const contentHeight = this.content.nativeElement.offsetHeight;
+  
+      // Calcular altura de la página
+      const body = document.body;
+      const html = document.documentElement;
+      this.pageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  
+      // Ajustar posición del footer
+      if (contentHeight < this.pageHeight) {
+        this.showFooter = true;
+      }
     }
   }
 
-  ngAfterViewInit() {
-    // Initialize the variable to true if the page is already in the final position
-    const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    
-    if (scrollTop >= height) {
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const scrollPosition = scrollY + windowHeight;
+
+    if (scrollPosition >= this.pageHeight) {
       this.showFooter = true;
+    } else {
+      this.showFooter = false;
     }
   }
 }
