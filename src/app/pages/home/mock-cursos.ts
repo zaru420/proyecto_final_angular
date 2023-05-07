2124a3a14595e5325a5c50955b171faf95a98b0d
Aaron <aaronzaru@gmail.com>
2023-05-07 23:09:09 +0200

    trabajando en arrays

---
+++ src/app/pages/home/mock-cursos.ts
@@ -0,0 +1,11 @@
+import { Curso } from "./curso";
+
+export const CURSOS: Curso[] = [
+ { 
+   id: "curso1",
+   nombre: "Curso de Front-End",
+   descripcion: "Curso de Front-End",
+   imgUrl: [ "../../../assets/imagenes/" ]
+ }
+ 
+];
