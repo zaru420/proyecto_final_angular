91531dba0c87ff62e712d48537041bbed7c60ea6
Aaron <aaronzaru@gmail.com>
2023-05-23 13:00:23 +0200

    Continuar en notas

--- src/app/pages/cursos/cursos.service.ts
+++ src/app/pages/cursos/cursos.service.ts
@@ -1,9 +1,62 @@
+// curso.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { Curso } from '../home/curso';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CursosService {
+export class CursoService {
+  private API_URL = 'http://localhost:8080/api/cursos';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Obtiene una lista de todos los cursos.
+   * @returns Devuelve un array de cursos.
+   */
+  getAll(): Observable<Curso[]> {
+    return this.http.get<Curso[]>(this.API_URL);
+  }
+
+  /**
+   * Busca cursos por nombre en la base de datos.
+   * @param nombre - Nombre del curso a buscar.
+   * @returns Devuelve los cursos con el mismo nombre.
+   */
+  getByNombre(nombre: string): Observable<Curso[]> {
+    const url = `${this.API_URL}?nombre=${nombre}`;
+    return this.http.get<Curso[]>(url);
+  }
+
+  /**
+   * Crea un nuevo curso.
+   * @param curso - Datos del curso a crear.
+   * @returns Devuelve los datos del curso guardados en la base de datos.
+   */
+  create(curso: Curso): Observable<Curso> {
+    return this.http.post<Curso>(this.API_URL, curso);
+  }
+
+  /**
+   * Actualiza un curso existente.
+   * @param id - ID del curso a actualizar.
+   * @param curso - Datos actualizados del curso.
+   * @returns Devuelve el curso actualizado.
+   */
+  update(id: number, curso: Curso): Observable<Curso> {
+    return this.http.put<Curso>(`${this.API_URL}/${id}`, curso);
+  }
+
+  /**
+   * Elimina un curso.
+   * @param id - ID del curso a eliminar.
+   * @returns En caso de error, devuelve una excepción con un mensaje.
+   */
+  delete(id: number): Observable<any> {
+    return this.http.delete(`${this.API_URL}/${id}`);
+  }
 }
+export { Curso };
+
---
+++ src/app/pages/cursos/cursos.ts
@@ -0,0 +1,34 @@
+
+export class Curso {
+
+    id!: number;
+    nombre!: string;
+    id_a1!: number;
+    id_a2!: number;
+    id_a3!: number;
+    id_a4!: number;
+    id_a5!: number;
+    id_a6!: number;
+    id_a7!: number;
+    id_a8!: number;
+    id_a9!: number;
+    id_a10!: number;
+
+    constructor() {
+
+      this.id = 0;
+      this.nombre = "";
+      this.id_a1 = 0;
+      this.id_a2 = 0;
+      this.id_a3 = 0;
+      this.id_a4 = 0;
+      this.id_a5 = 0;
+      this.id_a6 = 0;
+      this.id_a7 = 0;
+      this.id_a8 = 0;
+      this.id_a9 = 0;
+      this.id_a10 = 0;
+
+    }
+  }
+  
